bd8c18bffcdaeab0d7d1b6a63a2a00096f0cc72f
FazalulrehmanBelwadi <belwadifazalulrehman@gmail.com>
2022-03-04 12:48:05 +0530

    update

---
+++ MiniProject/3_Implementation/Texbook_view.c
@@ -0,0 +1,40 @@
+/**
+ * @file Texbook_view.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "library_management_system.h"
+/**
+ * @brief 
+ * 
+ * @return check_variable 
+ */
+check_variable Textbook_view()
+{
+
+    FILE *fp=NULL;
+    fp=fopen("bookLibrary.dat","rb");
+
+    if(fp==NULL)
+    {
+        printf("\nERROR:\n");
+        return fail;
+    }
+    else{
+        book *book_to_find=(book*)malloc(sizeof(book));
+    while(fread(book_to_find,sizeof(book),1,fp)==1)
+    {
+        printf("Textbook Id\t\t\tTextbook Name\t\t\tAuthor Name");
+        printf("\n%d\t\t\t\t%s\t\t\t\t%s",book_to_find->Textbook_id,book_to_find->Textbook_name,book_to_find->Author_name);
+    }
+    fclose(fp);
+    free(book_to_find);
+
+    return pass;
+    }
+}
--- MiniProject/3_Implementation/test.c
+++ MiniProject/3_Implementation/test.c
@@ -19,31 +19,51 @@
 void setUp(){}
 
 void tearDown(){}
-
+/**
+ * @brief 
+ * 
+ */
 
 void test_Textbook_Addon(void){
     TEST_ASSERT_EQUAL(pass,Textbook_Addon(125, "Travis", "Chalizard"));
 }
-
+/**
+ * @brief 
+ * 
+ */
 void test_Textbook_view(void){
     TEST_ASSERT_EQUAL(pass,Textbook_view());
 }
-
+/**
+ * @brief 
+ * 
+ */
 void test_Textbook_search(void){
     TEST_ASSERT_EQUAL(fail,searchbook(-2));
     TEST_ASSERT_EQUAL(pass,searchbook(125));
 }
+/**
+ * @brief 
+ * 
+ */
 
 
 
 void test_Textbook_Delete(void){
     TEST_ASSERT_EQUAL(pass,Textbook_Delete(125));
 }        
-
+/**
+ * @brief 
+ * 
+ */
 void test_Textbook_Issue(void){
     TEST_ASSERT_EQUAL(pass,Textbook_Issue(125));
 }
-
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
 int main(){
     UNITY_BEGIN();
 
